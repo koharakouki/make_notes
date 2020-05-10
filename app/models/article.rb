a3b155e25cdcc8b4b386e8fb94c9e74c4303055f
koharakouki <ko.kohara02@gmail.com>
2020-05-10 01:21:49 +0000

    [fix]記事ページのレイアウト修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
 
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
+	validates :article_title, presence: true, length: { maximum: 35 }
+	validates :content, presence: true
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
