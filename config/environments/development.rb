e94d2cca38665a3345823a9e5a7a459dbd686284
koharakouki <ko.kohara02@gmail.com>
2020-05-15 00:55:33 +0000

    [Add]letter_opener_web導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,17 +34,23 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => "gmail.com",
+  #   :user_name => ENV['USER_NAME'],
+  #   :password => ENV['PASS_WORD'],
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
+
+
+  # ActionMailer::Base.delivery_method = :letter_opener
+  # 開発環境では実際にメールは送らない
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :user_name => ENV['USER_NAME'],
-    :password => ENV['PASS_WORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  ActionMailer::Base.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
