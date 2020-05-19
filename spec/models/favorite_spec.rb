13dcc84a99727e847ed459fcca123692ffc3b146
koharakouki <ko.kohara02@gmail.com>
2020-05-19 19:52:10 +0000

    [Finish]コントローラのテスト完了

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
   describe 'バリデーションのテスト' do
-  	let!(:favorite) { create(:favorite) }
+    let!(:user) { create(:user) }
+    let!(:article) { create(:article, user_id: user.id) }
+  	let!(:favorite) { create(:favorite, user_id: user.id, article_id: article.id) }
 
   	it '項目が存在していれば有効' do
   		expect(favorite).to be_valid
