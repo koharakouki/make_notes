414cce8687564876095348744296b9cb75d0d8c9
koharakouki <ko.kohara02@gmail.com>
2020-05-27 05:11:35 +0000

    [Fix]定期メール機能修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  def everymonth_mail(user)
-    mail(from: 'make notes <makenotes2020@gmail.com>', to: user.email, subject: "make notesです")
+  def everymonth_mail
+    mail(from: 'make notes <makenotes2020@gmail.com>', bcc: User.pluck(:email), subject: "make notesです")
   end
 end
