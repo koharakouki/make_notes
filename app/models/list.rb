124377a92a40c69c609c5bc79b2ac78d0e454816
koharakouki <ko.kohara02@gmail.com>
2020-05-28 08:01:04 +0000

    [Fix]アイコン画像修正

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,6 +4,6 @@ class List < ApplicationRecord
 
   validates :media, length: { maximum: 20 }
   validates :title, presence: true, length: { maximum: 23 }
-  validates :genre_id, presence: true
+  # validates :genre_id, presence: true
   validates :user_id, presence: true
 end
