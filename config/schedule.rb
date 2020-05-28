6ea0644639cf7f0620540857f7f0e65a14bdc90d
koharakouki <ko.kohara02@gmail.com>
2020-05-28 13:20:46 +0000

    [Fix]定期メール機能

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,8 @@ set :output, 'log/cron.log'
 # ジョブの実行環境を本番環境に指定
 set :environment, :production
 
+ENV.each { |k, v| env(k, v) }
+
 every 1.month, at: 'start of the month at 0am' do
   runner 'UserMailer.everymonth_mail.deliver_now'
 end
